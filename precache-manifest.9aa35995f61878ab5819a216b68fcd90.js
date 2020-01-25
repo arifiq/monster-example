bfeda3a1c91dfdc030cc1e0f5b79d6ba3398f01d
arifiq <er.arifiqbal@gmail.com>
2020-01-26 03:20:22 +0530

    Updates

---
+++ precache-manifest.9aa35995f61878ab5819a216b68fcd90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdbe8c64bad79850af1c79e632d7687",
+    "url": "/monster-example/index.html"
+  },
+  {
+    "revision": "87e7eccb0c86c181ce2e",
+    "url": "/monster-example/static/css/main.a73e3f02.chunk.css"
+  },
+  {
+    "revision": "eff1a2204bd26c1723c9",
+    "url": "/monster-example/static/js/2.e2925cd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-example/static/js/2.e2925cd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87e7eccb0c86c181ce2e",
+    "url": "/monster-example/static/js/main.f1b4c639.chunk.js"
+  },
+  {
+    "revision": "1c022ffeb87c68bd3a9d",
+    "url": "/monster-example/static/js/runtime-main.2fb173ca.js"
+  }
+]);
